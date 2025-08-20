ab05f4bc9929cc885381931f88a37a327659b2e3
David Stratton <me@davidstratton.co.uk>
2025-08-20 09:36:46 +0100

    Intital Javascript validation Adjusted Contact Form Labels for easier styling Added Javascript to create Errors Checked for empty fields and display error.

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -12,9 +12,60 @@ menuButton.addEventListener('click', (e) => {
     siteSocial.classList.toggle('active');
 });
 
+/** 
+ *  Contact Form Validation
+*/
+
+const form = document.querySelector('.contact-form');
+const formInputs = document.querySelectorAll('.contact-form input, .contact-form textarea');
+
+const addError = (input, errormsg) => {
+    if (!input.nextElementSibling) {
+        input.classList.add('error');
+        let errorbox = document.createElement("span");
+        errorbox.classList.add("error-msg");
+        errorbox.textContent = errormsg;
+        input.insertAdjacentElement("afterend", errorbox);
+    } else {
+        errorbox.textContent = errormsg;
+    }
+}
+
+const isEmptyInput = () => {
+
+};
+
+form.addEventListener("input", (e) => {
+
+    e.target.classList.add('error');
+});
+
+form.addEventListener("submit", (e) => {
+    
+    let formReady = true;
+
+    for (let i = 0; i < formInputs.length; i++) {
+        if (!formInputs[i].value) {
+
+            addError(formInputs[i], "This field can not be empty.");
+
+            formReady = false;
+        }
+    }
+
+    if (!formReady) {
+        e.preventDefault();
+    } else {
+        // reset form errors.
+        // display success message.
+        // clear inputs.
+    }
+
+});
+
 
 /** 
- *  Sub-Header List Rotatior
+ *  Sub-Header List Rotator
  *   
 */
 
@@ -28,8 +79,6 @@ function rotateHeader(prev, current) {
 
     next = current + 1 >= listCount ? 0 : current + 1;
 
-    console.log(`listCount: ${listCount}, current: ${current}, next: ${next}`);
-    
     //Using setTimeout to call itself and repeat. To not block code execution.
     setTimeout(() => {
         rotateHeader(current, next);
