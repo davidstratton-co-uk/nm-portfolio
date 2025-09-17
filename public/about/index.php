ca1d6a4b743ca2c0a547223eb0df5b784af3cfe5
David Stratton <me@davidstratton.co.uk>
2025-09-17 12:23:30 +0100

    corrected incorrect folder seperators

--- public/about/index.php
+++ public/about/index.php
@@ -1,13 +1,13 @@
 <?php
 $page_title = '';
 $page_heading = '';
-$root_dir = '..\\';
-$base_dir = '..\\' . $root_dir;
+$root_dir = '..//';
+$base_dir = '..//' . $root_dir;
 
-    include $base_dir . 'app\\views\shared\head.view.php';
-    include $base_dir . 'app\\views\shared\\nav.view.php';
+    include $base_dir . 'app//views/shared/head.view.php';
+    include $base_dir . 'app//views/shared//nav.view.php';
 
-    include $base_dir . 'app\\views\about.view.php';
+    include $base_dir . 'app//views/about.view.php';
 
-    include $base_dir . 'app\\views\shared\\footer.view.php';
+    include $base_dir . 'app//views/shared//footer.view.php';
 ?>
